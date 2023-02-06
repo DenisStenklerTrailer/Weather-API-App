c1d4a0bca965f0c078e0345f44d989b87f8f602e
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-06 14:25:19 +0100

    Introduction to flask

---
+++ main.py
@@ -0,0 +1,17 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def home():
+    return flask.render_template("home.html")\
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
