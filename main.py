5e459d04179396c598e2b2ae4f3506fdb78c6607
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-07 13:12:13 +0100

    Added return for all data for the station and data of the station for the specific year

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ app = flask.Flask(__name__)
 stations = pd.read_csv("data/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
 
+# HOME PAGE
 @app.route("/")
 def home():
     return flask.render_template("home.html", data=stations.to_html())
 
+# RETURN TEMPERATURE OF STATION AND DATE
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt" # zfill nam naredi 000066
@@ -19,6 +21,23 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+# RETURN ALL DATA FOR THE STATION
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"  # zfill nam naredi 000066
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+# RETURN DATA OF THE STATION FOR THE SPECIFIC YEAR
+@app.route("/api/v1/year/<station>/<year>")
+def year_data(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"  # zfill nam naredi 000066
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
